3034ceb7b6669a52b13ba21a916f71a172c8ce58
mazienho <mazienho@gmx.de>
2015-08-28 23:19:16 +0200

    Removed logging option

--- synaptics_touchpad.sh
+++ synaptics_touchpad.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-LOGDATEI=/tmp/touchlog.log
 /usr/bin/synclient TapButton2=2
 /usr/bin/synclient TapButton3=3
-
-echo `date` > $LOGDATEI
-echo "Touchpad settings modified" >> $LOGDATEI
 
 exit 0
